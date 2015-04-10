89f204a76777377c2e69f735fb30552bbdf3246b
wveik <kataev_06@list.ru>
2015-04-10 16:42:28 +0300

    Первый commit

---
+++ LoginSystem/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace LoginSystem {
+    [Activity(Label = "LoginSystem", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity {
+
+        private Button mBtnSignUp;
+
+        protected override void OnCreate(Bundle bundle) {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            mBtnSignUp = FindViewById<Button>(Resource.Id.btnSignUpWithEmail);
+
+            mBtnSignUp.Click += (object sender, EventArgs args) => { 
+                //Pull up dialog
+                FragmentTransaction transaction = FragmentManager.BeginTransaction();
+                dialog_SignUp signUpDialog = new dialog_SignUp();
+                signUpDialog.Show(transaction, "dialog_fragment");
+            };
+        }
+
+
+    }
+}
+
---
+++ LoginSystem/dialog_SignUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.App;
+using Android.Views;
+using Android.OS;
+
+namespace LoginSystem {
+    public class dialog_SignUp : DialogFragment {
+
+        public override Android.Views.View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            
+            
+            base.OnCreateView(inflater, container, savedInstanceState);
+
+            var view = inflater.Inflate(Resource.Layout.dialog_sign_up, container, false);
+            return view;
+        }
+
+        public override void OnActivityCreated(Bundle savedInstanceState) {
+            Dialog.Window.RequestFeature(WindowFeatures.NoTitle); // Sets the title bar to invisible
+            
+            base.OnActivityCreated(savedInstanceState);
+
+            Dialog.Window.Attributes.WindowAnimations = Resource.Style.dialog_animation; // set the animate
+        }
+    }
+}
+
