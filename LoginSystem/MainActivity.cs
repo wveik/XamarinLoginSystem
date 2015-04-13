bfb8ea0b6c360b4e97aaf8324b713d067f73dc74
wveik <kataev_06@list.ru>
2015-04-13 14:19:38 +0300

    Добавил Event отправки

--- LoginSystem/MainActivity.cs
+++ LoginSystem/MainActivity.cs
@@ -5,12 +5,14 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using System.Threading;
 
 namespace LoginSystem {
-    [Activity(Label = "LoginSystem", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "Крутое приложение", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity {
 
         private Button mBtnSignUp;
+        private ProgressBar mProgressWait;
 
         protected override void OnCreate(Bundle bundle) {
             base.OnCreate(bundle);
@@ -19,15 +21,36 @@ namespace LoginSystem {
             SetContentView(Resource.Layout.Main);
 
             mBtnSignUp = FindViewById<Button>(Resource.Id.btnSignUpWithEmail);
+            mProgressWait = FindViewById<ProgressBar>(Resource.Id.progressBar1);
 
             mBtnSignUp.Click += (object sender, EventArgs args) => { 
                 //Pull up dialog
                 FragmentTransaction transaction = FragmentManager.BeginTransaction();
                 dialog_SignUp signUpDialog = new dialog_SignUp();
                 signUpDialog.Show(transaction, "dialog_fragment");
+
+                signUpDialog.mOnSignUpComplete += signUpDialog_mOnSignUpComplete;
             };
         }
 
+        void signUpDialog_mOnSignUpComplete(object sender, OnSignUpEventArgs e) {
+            
+            Thread thread = new Thread(ActLikeARequest);
+
+            thread.Start();
+        }
+
+        private void ActLikeARequest() {
+            RunOnUiThread(() => { 
+                mProgressWait.Visibility = ViewStates.Visible; 
+            });
+            
+            Thread.Sleep(3000);
+            
+            RunOnUiThread(() => {
+                mProgressWait.Visibility = ViewStates.Invisible;
+            });
+        }
 
     }
 }
--- LoginSystem/dialog_SignUp.cs
+++ LoginSystem/dialog_SignUp.cs
@@ -2,17 +2,51 @@
 using Android.App;
 using Android.Views;
 using Android.OS;
+using Android.Widget;
 
 namespace LoginSystem {
+
+    public class OnSignUpEventArgs : EventArgs {
+
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string Password { get; set; }
+
+        public OnSignUpEventArgs(string firstName, string email, string password) : base() {
+            Email = email;
+            FirstName = firstName;
+            Password = password;
+        }
+    }
+
     public class dialog_SignUp : DialogFragment {
 
-        public override Android.Views.View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            
-            
+        private EditText mTxtFirstName;
+        private EditText mTxtEmail;
+        private EditText mTxtPassword;
+        private Button mBtnSignUp;
+
+        public event EventHandler<OnSignUpEventArgs> mOnSignUpComplete;
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             base.OnCreateView(inflater, container, savedInstanceState);
 
             var view = inflater.Inflate(Resource.Layout.dialog_sign_up, container, false);
+
+            mTxtEmail = view.FindViewById<EditText>(Resource.Id.txtEmail);
+            mTxtFirstName = view.FindViewById<EditText>(Resource.Id.txtFirstName);
+            mTxtPassword = view.FindViewById<EditText>(Resource.Id.txtPassword);
+            mBtnSignUp = view.FindViewById<Button>(Resource.Id.btnDialogEmail);
+
+            mBtnSignUp.Click += mBtnSignUp_Click;
+
             return view;
+        }
+
+        void mBtnSignUp_Click(object sender, EventArgs e) {
+            //User has clicked sign up button
+            mOnSignUpComplete.Invoke(this, new OnSignUpEventArgs(mTxtFirstName.Text, mTxtEmail.Text, mTxtPassword.Text));
+            this.Dismiss();
         }
 
         public override void OnActivityCreated(Bundle savedInstanceState) {
